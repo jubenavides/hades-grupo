01a6f313187bd287e7f6b41f31d1ae1a16f3f350
jarm14 <joelram1@hotmail.com>
2017-11-27 20:54:49 -0500

    Mapeo clase menu

---
+++ hades-cruise-ejb/src/main/java/ec/edu/espe/distribuidas/hades/model/Menu.java
@@ -0,0 +1,113 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.distribuidas.hades.model;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author joel
+ */
+
+@Entity
+@Table(name="MENU")
+public class Menu {
+    
+    @Id
+    @Column(name="COD_ITEM",length = 5)
+    private Integer codigo;
+    @Column(name="NOMBRE",length = 100)
+    private String nombre;
+    @Column(name="DESCRIPCION",length = 300, nullable = false)
+    private String descripcion;
+    @Column(name="PRECIO",precision = 8, scale = 2)
+    private BigDecimal precio;
+    @Column(name="TIO",length = 5)
+    private String tipo;
+
+    public Menu() {
+    }
+
+    public Menu(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public BigDecimal getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(BigDecimal precio) {
+        this.precio = precio;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public String toString() {
+        return "Menu{" + "codigo=" + codigo + ", nombre=" + nombre + ", descripcion=" + descripcion + ", precio=" + precio + ", tipo=" + tipo + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 37 * hash + Objects.hashCode(this.codigo);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Menu other = (Menu) obj;
+        if (!Objects.equals(this.codigo, other.codigo)) {
+            return false;
+        }
+        return true;
+    } 
+}
