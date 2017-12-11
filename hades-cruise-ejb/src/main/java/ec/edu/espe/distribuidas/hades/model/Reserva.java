e5a8ca1be0be8052706296e47b039ddc78a95082
jose cajape <jscajape@espe.edu.ec>
2017-12-11 01:14:25 -0500

    Modificacion de las Clases Menu, MenuFacade, CruceroFacace

--- hades-cruise-ejb/src/main/java/ec/edu/espe/distribuidas/hades/model/Reserva.java
+++ hades-cruise-ejb/src/main/java/ec/edu/espe/distribuidas/hades/model/Reserva.java
@@ -5,26 +5,18 @@
  */
 package ec.edu.espe.distribuidas.hades.model;
 
-import com.sun.istack.internal.NotNull;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Date;
-import java.util.List;
-import javax.persistence.Basic;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinColumns;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import static javax.swing.text.StyleConstants.Size;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 /**
  *
