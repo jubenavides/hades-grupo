c4ceed45ca7490cc8942cc933ef1aaf671d52baf
Diego Pabon <dfpabon@espe.edu.ec>
2017-12-11 18:30:26 -0500

    Corrección clases Tour, TourPK y TourFacade.

--- hades-cruise-ejb/src/main/java/ec/edu/espe/distribuidas/hades/dao/TourFacade.java
+++ hades-cruise-ejb/src/main/java/ec/edu/espe/distribuidas/hades/dao/TourFacade.java
@@ -1,41 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ec.edu.espe.distribuidas.hades.dao;
 
 import ec.edu.espe.distribuidas.hades.model.Tour;
-import ec.edu.espe.distribuidas.hades.model.TuristaReserva;
 import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
-/**
- *
- * @author Hendrix
- */
 @Stateless
-public class TourFacade extends AbstractFacade<Tour> {
-
+public class TourFacade extends AbstractFacade<Tour>{
     @PersistenceContext(unitName = "ec.edu.espe.distribuidas.hades_hades-cruise-ejb_ejb_1PU")
     private EntityManager em;
-
     @Override
-    protected EntityManager getEntityManager() {
+    protected EntityManager getEntityManager(){
         return em;
     }
-
-    public TourFacade() {
+    public TourFacade(){
         super(Tour.class);
     }
-    
-    public List<Tour> findByReserva(String tipoTour) {
-        Query qry = this.em.createQuery("SELECT obj FROM Tour obj WHERE obj.tourPK.codTipoTour");
+    public List<Tour> findByTipo(String tipoTour){
+        Query qry=this.em.createQuery("SELECT obj FROM Tour obj WHERE obj.tourPK.codTipoTour");
         qry.setParameter(1, tipoTour);
         return qry.getResultList();
-    }
-    
+    } 
+    public List<Tour> findByCrucero(String crucero){
+        Query qry=this.em.createQuery("SELECT obj FROM Tour obj WHERE obj.tourPK.codCrucero");
+        qry.setParameter(1, crucero);
+        return qry.getResultList();
+    } 
 }
--- hades-cruise-ejb/src/main/java/ec/edu/espe/distribuidas/hades/model/TourPK.java
+++ hades-cruise-ejb/src/main/java/ec/edu/espe/distribuidas/hades/model/TourPK.java
@@ -1,92 +1,72 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ec.edu.espe.distribuidas.hades.model;
 
 import java.io.Serializable;
+import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 
-/**
- *
- * @author Hendrix
- */
 @Embeddable
-public class TourPK implements Serializable {
-
-    @Column(name = "COD_TOUR", nullable = false)
+public class TourPK implements Serializable{
+    @Column(name="COD_TOUR", nullable=false)
     private Integer codTour;
-    @Column(name = "COD_TIPO_TOUR", nullable = false, length = 10)
+    @Column(name="COD_TIPO_TOUR", nullable=false, length=10)
     private String codTipoTour;
-    @Column(name = "COD_CRUCERO", nullable = false)
+    @Column(name="COD_CRUCERO", nullable=false)
     private Integer codCrucero;
-
-    public TourPK() {
+    public TourPK(){
     }
-
-    public TourPK(Integer codTour, String codTipoTour, Integer codCrucero) {
-        this.codTour = codTour;
-        this.codTipoTour = codTipoTour;
-        this.codCrucero = codCrucero;
+    public TourPK(Integer codTour, String codTipoTour, Integer codCrucero){
+        this.codTour=codTour;
+        this.codTipoTour=codTipoTour;
+        this.codCrucero=codCrucero;
     }
-
-    public Integer getCodTour() {
+    public Integer getCodTour(){
         return codTour;
     }
-
-    public void setCodTour(Integer codTour) {
-        this.codTour = codTour;
+    public void setCodTour(Integer codTour){
+        this.codTour=codTour;
     }
-
-    public String getCodTipoTour() {
+    public String getCodTipoTour(){
         return codTipoTour;
     }
-
-    public void setCodTipoTour(String codTipoTour) {
-        this.codTipoTour = codTipoTour;
+    public void setCodTipoTour(String codTipoTour){
+        this.codTipoTour=codTipoTour;
     }
-
-    public Integer getCodCrucero() {
+    public Integer getCodCrucero(){
         return codCrucero;
     }
-
-    public void setCodCrucero(Integer codCrucero) {
-        this.codCrucero = codCrucero;
+    public void setCodCrucero(Integer codCrucero){
+        this.codCrucero=codCrucero;
     }
-
     @Override
-    public int hashCode() {
-        Integer hash = 0;
-        hash += (Integer) codTour;
-        hash += (codTipoTour != null ? codTipoTour.hashCode() : 0);
-        hash += (Integer) codCrucero;
+    public int hashCode(){
+        Integer hash=0;
+        hash+=(Integer) codTour;
+        hash+=(codTipoTour!=null?codTipoTour.hashCode():0);
+        hash+=(Integer)codCrucero;
         return hash;
     }
 
     @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof TourPK)) {
+    public boolean equals(Object obj){
+        if (this==obj){
+            return true;
+        }
+        if (obj==null){
             return false;
         }
-        TourPK other = (TourPK) object;
-        if (this.codTour != other.codTour) {
+        if (getClass()!=obj.getClass()){
             return false;
         }
-        if ((this.codTipoTour == null && other.codTipoTour != null) || (this.codTipoTour != null && !this.codTipoTour.equals(other.codTipoTour))) {
-            return false;
-        }
-        if (this.codCrucero != other.codCrucero) {
+        final TourPK other=(TourPK) obj;
+
+        if (!Objects.equals(this.codTour,other.codTour)){
             return false;
         }
         return true;
     }
-
     @Override
     public String toString() {
-        return "ec.edu.espe.distribuidas.hades.model.TourPK[ codTour=" + codTour + ", codTipoTour=" + codTipoTour + ", codCrucero=" + codCrucero + " ]";
+        return "TourPK{" + "codTour=" + codTour + ", codTipoTour=" + codTipoTour + ", codCrucero=" + codCrucero + '}';
     }
-    
 }
