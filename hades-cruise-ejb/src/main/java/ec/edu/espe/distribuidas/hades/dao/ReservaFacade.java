0a413c2ace22d0d20e8b7242c626060eff20613c
Distril8 <33971547+Distril8@users.noreply.github.com>
2017-12-11 01:37:30 -0500

    Agregacion del Query para Reservas
    Se agrego el query para conslutar la lista de reservas por cliente y tour.

--- hades-cruise-ejb/src/main/java/ec/edu/espe/distribuidas/hades/dao/ReservaFacade.java
+++ hades-cruise-ejb/src/main/java/ec/edu/espe/distribuidas/hades/dao/ReservaFacade.java
@@ -5,8 +5,11 @@
  */
 package ec.edu.espe.distribuidas.hades.dao;
 
+
 import ec.edu.espe.distribuidas.hades.model.Reserva;
+import java.util.List;
 import javax.ejb.Stateless;
+import javax.persistence.Query;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
@@ -28,5 +31,21 @@ public class ReservaFacade extends AbstractFacade<Reserva> {
     public ReservaFacade() {
         super(Reserva.class);
     }
-    
+     public List<Reserva> findByCliente(Integer identificacion, String codReserva) {
+        Query qry = this.em.createQuery("SELECT obj FROM Reserva obj WHERE obj.identificacion =?1 AND obj.codReserva = ?2");
+        qry.setParameter(1, identificacion);
+        qry.setParameter(2, codReserva);
+        return qry.getResultList();
+    }
+    public List<Reserva> findByTour(Integer codTour, String codTipoTour, Integer codCrucero,String codReserva) {
+        Query qry = this.em.createQuery("SELECT obj FROM Reserva obj WHERE "
+                + "obj.codReserva = ?1 "
+                + "AND obj.codTour = ?2 AND obj.CodTipoTour = ?3"
+                + "AND objR.codCrucer = ?4 ");
+        qry.setParameter(1, codReserva);
+        qry.setParameter(2, codTour);
+        qry.setParameter(3, codTipoTour);
+        qry.setParameter(4, codCrucero);
+        return qry.getResultList();
+    }
 }
